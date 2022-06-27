7c97998434106adf7bc40d7c2269c9d82737a69b
Safwaan <69767323+notsafwaan@users.noreply.github.com>
2022-06-27 23:48:32 +0530

    441. Arranging Coins

---
+++ Day 19/ArrangeCoins.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/arranging-coins/
+class ArrangeCoins {
+    public int arrangeCoins(int n) {
+        int lo = 1, hi = n, mid;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (mid * (mid + 1L) <= n * 2L) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return hi;
+    }
+}
