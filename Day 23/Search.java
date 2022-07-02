e9b8e13d4875ea5930ab841dc74421fae2f48557
Safwaan <69767323+notsafwaan@users.noreply.github.com>
2022-07-02 22:59:41 +0530

    1662. Check If Two String Arrays are Equivalent

---
+++ Day 23/Search.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+class Search {
+    public int search(int[] nums, int target) {
+        if (nums== null || nums.length == 0) return -1;
+        int pivot = getPivot(nums);
+        int lo = 0, hi = nums.length - 1;
+        if (target <= nums[hi]) lo = pivot;
+        else hi = pivot - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return -1;
+    }
